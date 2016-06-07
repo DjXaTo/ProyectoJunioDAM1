6698a07154a1e5c6519e8deb702f2ca08d1bea09
Oreo <Oreo@192.168.1.36>
2016-06-07 13:05:59 +0200

    Versión 20160604 1305

--- src/Controlador/main.java
+++ src/Controlador/main.java
@@ -1,3 +1,10 @@
-public class main {
+package controlador;
 
+import Vista.interfaz;
+
+public class main {
+    
+    public static void main(String[] args){
+        new controlador(new interfaz()).iniciar();
+    }
 }
